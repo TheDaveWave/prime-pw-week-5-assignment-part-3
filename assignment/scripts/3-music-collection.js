725c691492ae6603c37326b9e939a9dd3dcbc2f0
David Lindberg <david.wesley.lindberg@gmail.com>
2022-08-18 13:06:18 -0500

    made addToCollection add an object to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,10 @@ console.log('***** Music Collection *****')
 const collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-
+    let album = {};
+    album.title = title;
+    album.artist = artist;
+    album.yearPublished = yearPublished;
+    collection.push(album);
+    return album;
 }
